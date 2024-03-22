e415ab3030a425905acc744f535f2b847ea5edd9
AgusPaez <agus.123.paez@gmail.com>
2024-03-22 15:38:14 -0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,98 @@
 import React from 'react';
 //images
-import suinfiHero from '../assets/suinfiHero.png';
-import phone from '../assets/Phone.png';
-import place from '../assets/Place Marker.png';
+// import suinfiHero from '../assets/suinfiHero.png';
+// import phone from '../assets/Phone.png';
+// import place from '../assets/Place Marker.png';
 export const Footer = () => {
   return (
-    <footer className="bg-[#342b53] containerWidth rounded-bl-none rounded-br-none mx-auto rounded-[80px] overflow-hidden h-[600px] lg:h-[300px]">
-      <div className="h-full">
-        <div className="w-full h-full items-center text-white flex flex-wrap justify-between mx-auto lg:mx-0">
-          <div className="ml-5 flex gap-5 justify-center relative items-center ">
-            <div className="flex flex-col justify-center items-center mt-4">
-              <div className="w-[150px] mb-2 mt-[-20px]">
-                <img src={suinfiHero} alt="SUINFI" className="" />
-              </div>
-              <div className="bl-2 w-[200px] text-xl text-center">
-                <h2>e c o m m e r c e</h2>
-              </div>
+    <footer className="bg-gradient-to-t from-[#344A65] to-[#396691] mx-auto overflow-hidden h-1/3 h-50 lg:h-80">
+      <div className="w-full h-full mt-8 text-white flex flex-wrap text-[9px] sm:text-xs lg:text-base">
+        <div className="lg:mx-14 lg:my-6 sm:mx-7 sm:my-3 flex justify-bettwen lg:gap-5 w-full h-auto">
+          <div className="flex flex-col gap-5 w-1/4 m-0.5 ml-1 lg:m-2">
+            <h2 className="font-semibold lg:m-2 lg:mx-2 my-0">
+              Redes Sociales
+            </h2>
+            <div className="flex flex-col gap-1 lg:mx-2 my-0">
+              <a href="#" className="hover:underline">
+                Instagram
+              </a>
+              <a href="#" className="hover:underline">
+                Twitter
+              </a>
+              <a href="#" className="hover:underline">
+                Facebook
+              </a>
+              <a href="#" className="hover:underline">
+                Youtube
+              </a>
             </div>
           </div>
-
-          <div className="ml-5 flex flex-col gap-0.25 items-center">
-            <div className="w-[250px] mb-0 flex items-center">
-              <img src={phone} alt="telefono" className="w-4 h-4 mr-2" />
-              <b className="">Teléfono</b>
-            </div>
-            <div className="w-[250px] mb-0 flex items-center">
-              <p className="ml-6">+54 9 2644585555</p>
-            </div>
-            <div className="w-[250px] mb-0 flex items-center">
-              <img src={place} alt="visit" className="w-4 h-4 mr-2" />
-              <b className="">Visitanos</b>
-            </div>
-            <div className="w-[250px] mb-0 flex items-center">
-              <p className="ml-6">Rivadavia 524 (E) San Juan</p>
+          <div className="flex flex-col gap-8 sm:gap-8 md:gap-8 lg:gap-8 m-0.5 lg:m-2 w-1/4">
+            <h2 className="font-semibold lg:m-2">Acerca de</h2>
+            <div className="flex flex-col gap-1 mx-2 my-0">
+              <a href="#" className="hover:underline">
+                SUINFI
+              </a>
+              <a href="#" className="hover:underline">
+                Tendencias
+              </a>
+              <a href="#" className="hover:underline">
+                Blog
+              </a>
             </div>
           </div>
-          <div className="ml-5 flex flex-col gap-0.25 items-center">
-            <div className="w-[250px] mb-0 flex items-center">
-              <img src={phone} alt="telefono" className="w-4 h-4 mr-2" />
-              <b className="">Dubai Phone</b>
+          <div className="flex flex-col md:gap-8 gap-8 lg:gap-8 m-0.5 lg:m-2 w-1/4">
+            <h2 className="font-semibold lg:m-2">Otros Sitios</h2>
+            <div className="flex flex-col gap-1 mx-2 my-0">
+              <a href="#" className="hover:underline">
+                Software Factory
+              </a>
+              <a href="#" className="hover:underline">
+                Ecommerce
+              </a>
             </div>
-            <div className="w-[250px] mb-0 flex items-center">
-              <p className="ml-6">+971 54 231 6393</p>
+          </div>
+          <div className="flex flex-col gap-5 sm:gap-8 md:gap-8 lg:gap-2 xl:gap-8 m-0.5 lg:m-2 w-1/3">
+            <h2 className="font-semibold lg:m-2">Contacto en Argentina</h2>
+            <div className="flex flex-col gap-1 mx-2 my-0">
+              <a href="#" className="hover:underline">
+                Teléfono:+54 9 264 4585555
+              </a>
+              <a href="#" className="hover:underline">
+                Visitanos: Rivadavia 524 (E) San Juan-Argentina
+              </a>
             </div>
-            <div className="w-[250px] mb-0 flex items-center">
-              <img src={place} alt="visit" className="w-4 h-4 mr-2" />
-              <b className="">Visit us</b>
+          </div>
+          <div className="flex flex-col gap-8 m-0.5 lg:m-2 w-1/3">
+            <h2 className="font-semibold lg:m-2">Contacto en UAE</h2>
+            <div className="flex flex-col gap-1 mx-2 my-0">
+              <a href="#" className="hover:underline">
+                Teléfono:+971 54 231 6393
+              </a>
+              <a href="#" className="hover:underline">
+                Visitanos: Jumeirah Lake Towers, Arch Tower G - Dubai - United
+                Arab Emirates
+              </a>
             </div>
-            <div className="w-[250px] mb-0 flex items-center">
-              <p className="ml-6">
-                Jumeirah Lake Towers, Arch Tower G - Dubái - United Arab
-                Emirates
-              </p>
+          </div>
+          {/* <div className="flex flex-col gap-8 w-1/4 m-0.5 ml-1 lg:m-2">
+            <h2 className="font-semibold lg:m-2 lg:mx-2 my-0">
+              Redes Sociales */}
+          <div className="flex flex-col gap-8 md:gap-8 lg:gap-8 m-0.5 mr-1 lg:m-2 w-1/3">
+            <h2 className="font-semibold lg:m-2">Ayuda</h2>
+            <div className="flex flex-col gap-1 mx-2 my-0">
+              <a href="#" className="hover:underline">
+                Comprar
+              </a>
+              <a href="#" className="hover:underline">
+                Vender
+              </a>
+              <a href="#" className="hover:underline">
+                Resolucion de problemas
+              </a>
+              <a href="#" className="hover:underline">
+                Centro de seguridad
+              </a>
             </div>
           </div>
         </div>
