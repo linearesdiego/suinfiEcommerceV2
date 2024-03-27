eb01ab75150734a781123cd056fbee03fbc6aeb2
Santiago Servera <santiagoservera21@gmail.com>
2024-03-27 18:22:21 -0300

    page and component YourProduct responsive

--- src/components/yourProducts/YourProductsComp.jsx
+++ src/components/yourProducts/YourProductsComp.jsx
@@ -9,71 +9,102 @@ import productType from '../../assets/productType.png';
 
 export const YourProductsComp = () => {
   return (
-    <div className="lg:w-full lg:flex lg:flex-row lg:h-full min-h-screen bgnewProduct">
-      <div className="lg:w-[25%] lg:h-full lg:flex lg:items-center ">
-        <aside className="text-white bg-sidebar lg:min-h-screen  lg:h-full lg:w-full lg:my-auto">
-          <div className="max-w-[90%] mx-auto pt-7">
+    <div className="lg:w-full flex flex-row lg:h-full min-h-screen bgnewProduct">
+      <div className="w-full lg:w-[25%] lg:h-full lg:flex lg:items-center ">
+        <aside className="text-white w-full bg-sidebar lg:min-h-screen  lg:h-full lg:w-full lg:my-auto mr-10">
+          <div className="max-w-[90%] mx-auto pt-7 ">
             <input
               type="text"
               name=""
               id=""
               placeholder="BUSCAR EN SUINFI"
-              className="w-full h-7 rounded-lg text-center"
+              className="w-full h-7 rounded-lg text-center hidden lg:block"
             />
           </div>
-          <div className="flex flex-col gap-3 pt-5">
-            <div className="flex flex-row items-center px-5">
-              <img src={ExplorarImg} alt="" className="w-[17px] h-[17px]" />
-              <p className="px-5 py-3">EXPLORAR</p>
+          <div className="flex flex-col gap-3 pt-5 lg:items-start  items-center">
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={ExplorarImg}
+                alt=""
+                className="lg:w-[17px] lg:h-[17px] w-[25px] h-[25px]"
+              />
+              <p className="px-5 py-3 hidden lg:block">EXPLORAR</p>
             </div>
-            <div className="flex flex-row items-center px-5">
-              <img src={ComprarImg} alt="" className="w-[17px] h-[17px]" />
-              <p className="px-5 py-3">COMPRAR</p>
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={ComprarImg}
+                alt=""
+                className="lg:w-[17px] lg:h-[17px] w-[25px] h-[25px]"
+              />
+              <p className="px-5 py-3 hidden lg:block">COMPRAR</p>
             </div>
-            <div className="flex flex-row items-center px-5">
-              <img src={TusComprasImg} alt="" className="w-[17px] h-[17px]" />
-              <p className="px-5 py-3">TUS VENTAS</p>
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={TusComprasImg}
+                alt=""
+                className="lg:w-[17px] lg:h-[17px] w-[25px] h-[25px] "
+              />
+              <p className="px-5 py-3 hidden lg:block">TUS VENTAS</p>
             </div>
-            <div className="flex flex-row items-center px-5">
-              <img src={FiltrosImg} alt="" className="w-[17px] h-[17px]" />
-              <p className="px-5 py-3">FILTROS</p>
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={FiltrosImg}
+                alt=""
+                className="lg:w-[17px] lg:h-[17px] w-[25px] h-[25px]"
+              />
+              <p className="px-5 py-3 hidden lg:block">FILTROS</p>
             </div>
           </div>
-          <div className="mt-7 max-w-[90%] mx-auto bg-[#27425E] rounded-lg py-1">
-            <button className="w-full">+ CREAR NUEVO PRODUCTO</button>
+          <div className="mt-7 max-w-[90%] mx-auto bg-[#27425E] rounded-lg py-1 mb-5">
+            <button className="w-full ">
+              <p className="hidden lg:block">+ CREAR NUEVO PRODUCTO</p>
+              <p className="lg:hidden">+</p>
+            </button>
           </div>
-          <div className="mt-7 p-5">
+          <div className="mt-7 p-5 hidden lg:block">
             <p className="text-lg font-semibold">CATEGORIAS</p>
           </div>
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-row items-center px-5">
-              <img src={productType} alt="" />
+          <div className="flex flex-col gap-3 pt-5 lg:items-start  items-center">
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={productType}
+                alt=""
+                className=" w-[25px] h-[25px] lg:w-[17px] lg:h-[17px]"
+              />
 
-              <p className="px-5 py-3">PRODUCTO 1</p>
+              <p className="px-5 py-3 hidden lg:block">PRODUCTO 1</p>
             </div>
-            <div className="flex flex-row items-center px-5">
-              <img src={productType} alt="" />
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={productType}
+                alt=""
+                className=" w-[25px] h-[25px] lg:w-[17px]  lg:h-[17px] "
+              />
 
-              <p className="px-5 py-3">PRODUCTO 2</p>
+              <p className="px-5 py-3 hidden lg:block">PRODUCTO 2</p>
             </div>
-            <div className="flex flex-row items-center px-5">
-              <img src={productType} alt="" />
+            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+              <img
+                src={productType}
+                alt=""
+                className=" w-[25px] h-[25px] lg:w-[17px] lg:h-[17px] "
+              />
 
-              <p className="px-5 py-3">PRODUCTO 3</p>
+              <p className="px-5 py-3 hidden lg:block">PRODUCTO 3</p>
             </div>
           </div>
         </aside>
       </div>
 
-      <div className="lg:w-full lg:h-full  lg:p-5 lg:flex lg:flex-row ">
-        <section className="lg:w-full lg:h-full  ">
-          <div className="lg:max-w-[1300px] lg:mx-auto lg:my-5 flex flex-row justify-evenly ">
+      <div className="lg:w-full lg:h-full  lg:p-5 lg:flex  ">
+        <section className="lg:w-full lg:h-full w-full h-full  ">
+          <div className="lg:max-w-[1300px] max-w-[600px] lg:mx-auto lg:my-5 lg:flex lg:flex-row lg:justify-evenly flex flex-col  mx-10 mt-10 gap-5">
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
           </div>
-          <div className="lg:max-w-[1300px] lg:mx-auto lg:my-5 flex flex-row justify-evenly ">
+          <div className="lg:max-w-[1300px] max-w-[600px] lg:mx-auto lg:my-5 lg:flex lg:flex-row lg:justify-evenly flex flex-col  mx-10 mt-10 gap-5">
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
             <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
