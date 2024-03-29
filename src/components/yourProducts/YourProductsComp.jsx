aa4f38d4468aa58302d7ddf7e22a063863336b5a
Santiago Servera <santiagoservera21@gmail.com>
2024-03-29 14:29:27 -0300

    yourProducts sections separates

--- src/components/yourProducts/YourProductsComp.jsx
+++ src/components/yourProducts/YourProductsComp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Imgs imports
 import ExplorarImg from '../../assets/Explorar.png';
@@ -8,6 +8,14 @@ import FiltrosImg from '../../assets/filtros.png';
 import productType from '../../assets/productType.png';
 
 export const YourProductsComp = () => {
+  // Estado local para controlar la sección activa
+  const [activeSection, setActiveSection] = useState(''); // Por defecto, la sección 'explorar' está activa
+
+  // Función para cambiar la sección activa cuando se hace clic en un ícono del aside
+  const handleSectionChange = (section) => {
+    setActiveSection(section);
+  };
+
   return (
     <div className="lg:w-full flex flex-row lg:h-full min-h-screen bgnewProduct">
       <div className="w-full lg:w-[25%] lg:h-full lg:flex lg:items-center ">
@@ -21,8 +29,14 @@ export const YourProductsComp = () => {
               className="w-full h-7 rounded-lg text-center hidden lg:block"
             />
           </div>
-          <div className="flex flex-col gap-3 pt-5 lg:items-start  items-center">
-            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+
+          <div className="flex flex-col gap-3 pt-5 lg:items-start items-center">
+            <div
+              className={`flex flex-row items-center lg:px-5 lg:p-0 py-4 cursor-pointer w-full hover:bg-[#243C5B80] ${
+                activeSection === 'explorar' && 'bg-[#2D5682]'
+              }`}
+              onClick={() => handleSectionChange('explorar')}
+            >
               <img
                 src={ExplorarImg}
                 alt=""
@@ -38,7 +52,12 @@ export const YourProductsComp = () => {
               />
               <p className="px-5 py-3 hidden lg:block">COMPRAR</p>
             </div>
-            <div className="flex flex-row items-center lg:px-5 lg:p-0 py-4">
+            <div
+              className={`flex flex-row items-center lg:px-5 lg:p-0 py-4 cursor-pointer w-full hover:bg-[#243C5B80] ${
+                activeSection === 'tus ventas' && 'bg-[#2D5682]'
+              }`}
+              onClick={() => handleSectionChange('tus ventas')}
+            >
               <img
                 src={TusComprasImg}
                 alt=""
@@ -97,20 +116,32 @@ export const YourProductsComp = () => {
       </div>
 
       <div className="lg:w-full lg:h-full  lg:p-5 lg:flex  ">
-        <section className="lg:w-full lg:h-full w-full h-full  ">
-          <div className="lg:max-w-[1300px] max-w-[600px] lg:mx-auto lg:my-5 lg:flex lg:flex-row lg:justify-evenly flex flex-col  mx-10 mt-10 gap-5">
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+        {/* Explorar section */}
+        {activeSection === 'explorar' && (
+          <section className="lg:w-full lg:h-full w-full h-full  ">
+            <div className="lg:max-w-[1300px] max-w-[600px] lg:mx-auto lg:my-5 lg:flex lg:flex-row lg:justify-evenly flex flex-col  mx-10 mt-10 gap-5">
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+            </div>
+            <div className="lg:max-w-[1300px] max-w-[600px] lg:mx-auto lg:my-5 lg:flex lg:flex-row lg:justify-evenly flex flex-col  mx-10 mt-10 gap-5">
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+              <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
+            </div>
+          </section>
+        )}
+        {/* Tus ventas section */}
+        {activeSection === 'tus ventas' && (
+          <div className="w-full flex flex-col gap-4">
+            <div>
+              <h1 className="text-3xl font-semibold">Tus Publicaciones</h1>
+            </div>
+            <div className="bg-white w-full h-[350px] rounded-lg shadow-lg"></div>
           </div>
-          <div className="lg:max-w-[1300px] max-w-[600px] lg:mx-auto lg:my-5 lg:flex lg:flex-row lg:justify-evenly flex flex-col  mx-10 mt-10 gap-5">
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-            <div className="w-[280px] h-[385px] bg-white border border-[#00000024] rounded-lg shadow-lg"></div>
-          </div>
-        </section>
+        )}
       </div>
     </div>
   );
