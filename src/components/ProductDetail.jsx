9808b4e2564d5ab34bbc8e1dd545e1491f53f48e
bautielipinali <elipinalibauti1@gmail.com>
2024-04-09 15:33:48 -0300

    Completed creation of 'payments' and 'reviewproducts' components, including design and responsiveness. Fixed header in the navbar.

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Loader } from './Loader';
 import { Navbar } from './Navbar';
+import { ReviewProduct } from './ReviewProduct';
+import { Payments } from './Payments';
 import { useAuth } from '../context/Auth';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { addProductToCart } from '../services/Carrito';
@@ -80,45 +82,52 @@ const ProductDetail = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div className="bg-gradient-to-tr from-[#9378a5] to-[#f6eef9] w-full h-full lg:h-[100vh] flex items-center justify-center">
-        <div className="container-width-ecommerce lg:flex lg:flex-row justify-between items-center mt-20 lg:mt-0">
+      <div className="w-full h-full lg:h-[100vh] flex items-center justify-center flex-col">
+        <div className="lg:flex lg:flex-row justify-between items-center mt-20 lg:mt-0">
           {isLoading && <Loader />}
           {!isLoading && (
-            <div className="flex flex-col lg:flex-row justify-center items-center gap-5 mt-10">
-              <div className="">
-                <div className="lg:w-[500px] lg:h-[500px] w-[250px] h-[250px]">
-                  <img
-                    src={product?.imagenNew}
-                    alt="article img"
-                    className="rounded-lg "
-                  />
-                </div>
-              </div>
-              <div className="w-[50%] flex flex-col gap-9">
-                <h2 className="text-2xl lg:text-4xl font-bold">
-                  {product?.nombre}
-                </h2>
-                <p className="-mt-8">
-                  {product?.descripcion}Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Vestibulum dictum laoreet ipsum, ac efficitur
-                  erat tempus ut. Nam ut aliquet metus, sit amet.
-                </p>
-                <h2 className="text-4xl font-bold">${product?.precio}</h2>
-                <div className="flex flex-col gap-5 pb-10 lg:flex-row ">
-                  <div className="flex w-full lg:w-[25%] flex-row mr-7 justify-center bg-[#f6eef9] gap-3 items-center px-0  rounded-xl font-semibold text-center">
-                    <button onClick={handleDecrement}>-</button>
-
-                    <p className="w-4">{cantidad}</p>
-                    <button onClick={handleIncrement}>+</button>
+            <div className="flex flex-col containerWidth">
+              <div className="flex flex-col lg:flex-row justify-center items-center gap-5 lg:mt-[60rem]">
+                <div className="">
+                  <div className="lg:w-[500px] lg:h-[500px] w-[250px] h-[250px]">
+                    <img
+                      src={product?.imagenNew}
+                      alt="article img"
+                      className="rounded-lg "
+                    />
                   </div>
-                  <button
-                    onClick={handleAddToCart}
-                    className="bg-black text-white px-10 py-2 rounded-full focus:shadow-2xl"
-                  >
-                    AGREGAR AL CARRITO
-                  </button>
+                </div>
+                <div className="w-[50%] flex flex-col gap-9">
+                  <h2 className="text-2xl lg:text-4xl font-bold">
+                    {product?.nombre}
+                  </h2>
+                  <p className="-mt-5">
+                    {product?.descripcion}Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Vestibulum dictum laoreet
+                    ipsum, ac efficitur erat tempus ut. Nam ut aliquet metus,
+                    sit amet.
+                  </p>
+                  <h2 className="text-4xl font-bold">${product?.precio}</h2>
+                  <div className="flex flex-col gap-5 pb-10 lg:flex-row ">
+                    <div className="flex w-full lg:w-[6rem] flex-row mr-7 justify-center bg-none gap-3 items-center px-0 border-[1px] border-[#000000] rounded-xl font-semibold text-center">
+                      <button onClick={handleDecrement} className=''>-</button>
+
+                      <p className="min-w-4">{cantidad}</p>
+                      <button onClick={handleIncrement}>+</button>
+                    </div>
+                    <button
+                      onClick={handleAddToCart}
+                      className="bg-black text-white px-10 py-2 rounded-full focus:shadow-2xl"
+                    >
+                      AGREGAR AL CARRITO
+                    </button>
+                  </div>
                 </div>
               </div>
+              <Payments/>
+              <div className="h-[1px] w-full bg-[#b3b3b3] my-24"></div>
+              <ReviewProduct />
+              <div className="h-[1px] w-full bg-[#b3b3b3] my-24"></div>
             </div>
           )}
         </div>
